b3b62d2d22d2d6720fb0244588aa18a4a9a8d897
Hassan Andrabi <handrabi53@gmail.com>
2022-10-05 00:05:50 +1100

    New batch of models.

--- main.py
+++ main.py
@@ -2,38 +2,72 @@ from keras.layers import LSTM, Bidirectional, Dense, Conv1D, MaxPooling1D, Globa
 from src.estimate import estimate
 
 # All models feature an untrainable embedding layer by default
+# models = {
+# 	"1hl" : [
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Flatten(),
+# 		Dense(1, activation="sigmoid"),
+# 	],
+# 	"2hl" : [
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Flatten(),
+# 		Dense(1, activation="sigmoid"),
+# 	],
+# 	"3hl" : [
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Flatten(),
+# 		Dense(1, activation="sigmoid"),
+# 	],
+# 	"4hl" : [
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Flatten(),
+# 		Dense(1, activation="sigmoid"),
+# 	],
+# 	"conv_maxPool_1hl" : [
+# 		Conv1D(128, 5, activation="relu"),
+# 		MaxPooling1D(5),
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Flatten(),
+# 		Dense(1, activation="sigmoid"),
+# 	],
+# 	"conv_maxPool_2hl" : [
+# 		Conv1D(128, 5, activation="relu"),
+# 		MaxPooling1D(5),
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Flatten(),
+# 		Dense(1, activation="sigmoid"),
+# 	],
+# 	"biDirectional_1hl" : [
+# 		Bidirectional(LSTM(100, activation="relu", kernel_regularizer="l2")),
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Flatten(),
+# 		Dense(1, activation="sigmoid"),
+# 	],
+# 	"biDirectional_2hl" : [
+# 		Bidirectional(LSTM(100, activation="relu", kernel_regularizer="l2")),
+# 		Dense(128, activation="relu", kernel_regularizer="l2"),
+# 		Flatten(),
+# 		Dense(1, activation="sigmoid"),
+# 	],
+# }
+
 models = {
-	"1hl" : [
-		Dense(128, activation="relu", kernel_regularizer="l2"),
-		Flatten(),
-		Dense(1, activation="sigmoid"),
-	],
-	"2hl" : [
-		Dense(128, activation="relu", kernel_regularizer="l2"),
-		Dense(128, activation="relu", kernel_regularizer="l2"),
-		Flatten(),
-		Dense(1, activation="sigmoid"),
-	],
-	"3hl" : [
-		Dense(128, activation="relu", kernel_regularizer="l2"),
-		Dense(128, activation="relu", kernel_regularizer="l2"),
-		Flatten(),
-		Dense(1, activation="sigmoid"),
-	],
-	"4hl" : [
-		Dense(128, activation="relu", kernel_regularizer="l2"),
-		Dense(128, activation="relu", kernel_regularizer="l2"),
-		Flatten(),
-		Dense(1, activation="sigmoid"),
-	],
-	"conv_maxPool_1hl" : [
+	"2conv_2maxPool_1hl" : [
+		Conv1D(128, 5, activation="relu"),
+		MaxPooling1D(5),
 		Conv1D(128, 5, activation="relu"),
 		MaxPooling1D(5),
 		Dense(128, activation="relu", kernel_regularizer="l2"),
 		Flatten(),
 		Dense(1, activation="sigmoid"),
 	],
-	"conv_maxPool_2hl" : [
+	"2conv_2maxPool_2hl" : [
+		Conv1D(128, 5, activation="relu"),
+		MaxPooling1D(5),
 		Conv1D(128, 5, activation="relu"),
 		MaxPooling1D(5),
 		Dense(128, activation="relu", kernel_regularizer="l2"),
@@ -41,37 +75,61 @@ models = {
 		Flatten(),
 		Dense(1, activation="sigmoid"),
 	],
-	"biDirectional_1hl" : [
-		Bidirectional(LSTM(100, activation="relu", kernel_regularizer="l2")),
+	"3conv_3maxPool_1hl" : [
+		Conv1D(128, 5, activation="relu"),
+		MaxPooling1D(5),
+		Conv1D(128, 5, activation="relu"),
+		MaxPooling1D(5),
+		Conv1D(128, 5, activation="relu"),
 		Dense(128, activation="relu", kernel_regularizer="l2"),
 		Flatten(),
 		Dense(1, activation="sigmoid"),
 	],
-	"biDirectional_2hl" : [
-		Bidirectional(LSTM(100, activation="relu", kernel_regularizer="l2")),
+	"3conv_3maxPool_2hl" : [
+		Conv1D(128, 5, activation="relu"),
+		MaxPooling1D(5),
+		Conv1D(128, 5, activation="relu"),
+		MaxPooling1D(5),
+		Conv1D(128, 5, activation="relu"),
+		Dense(128, activation="relu", kernel_regularizer="l2"),
 		Dense(128, activation="relu", kernel_regularizer="l2"),
 		Flatten(),
 		Dense(1, activation="sigmoid"),
 	],
-	# "1_bi_directional" : [
-	# 	Bidirectional(LSTM(100, activation="relu")),
-	# 	Dense(128, activation="relu"),
-	# 	Dense(1, activation="sigmoid"),
-	# ],
-	# "2_bi_directional" : [
-	# 	Bidirectional(LSTM(100, return_sequences=True, activation="relu")),
-	# 	Bidirectional(LSTM(100, activation="relu")),
-	# 	Dense(128, activation="relu"),
-	# 	Dense(1, activation="sigmoid"),
-	# ],
+	"3conv_2maxPool_globalMaxPool_1hl" : [
+		Conv1D(128, 5, activation="relu"),
+		MaxPooling1D(5),
+		Conv1D(128, 5, activation="relu"),
+		MaxPooling1D(5),
+		Conv1D(128, 5, activation="relu"),
+		GlobalMaxPooling1D(),
+		Dense(128, activation="relu", kernel_regularizer="l2"),
+		Flatten(),
+		Dense(1, activation="sigmoid"),
+	],
+	"3conv_2maxPool_globalMaxPool_2hl" : [
+		Conv1D(128, 5, activation="relu"),
+		MaxPooling1D(5),
+		Conv1D(128, 5, activation="relu"),
+		MaxPooling1D(5),
+		Conv1D(128, 5, activation="relu"),
+		GlobalMaxPooling1D(),
+		Dense(128, activation="relu", kernel_regularizer="l2"),
+		Dense(128, activation="relu", kernel_regularizer="l2"),
+		Flatten(),
+		Dense(1, activation="sigmoid"),
+	],
 }
 
 for name, layers in models.items():
 	print("\n_________________________________________________________________")
 	print(f"Estimating model: {name}")
 	print("=================================================================\n")
-	estimate(
-		name=name,
-		layers=layers,
-		epochs=20,
-	)
+	try:
+		estimate(
+			name=name,
+			layers=layers,
+			epochs=20,
+		)
+	except Exception as e:
+		print(e)
