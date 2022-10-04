03a7f1964f56edefe680d8b997950fe6faf0e230
Hassan Andrabi <handrabi53@gmail.com>
2022-10-05 00:14:50 +1100

    Add logging system.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from keras.layers import LSTM, Bidirectional, Dense, Conv1D, MaxPooling1D, GlobalMaxPooling1D, Flatten
 from src.estimate import estimate
+from datetime import datetime
+import sys
+
+stdout_file = f"logs/{datetime.now().strftime('%d-%m-%Y_%H:%M:%S')}"
+print(f"Writing output to: {stdout_file}.")
+sys.stdout = open(stdout_file, "w")
 
 # All models feature an untrainable embedding layer by default
 # models = {
@@ -133,3 +139,5 @@ for name, layers in models.items():
 		)
 	except Exception as e:
 		print(e)
+
+sys.stdout.close()
