99292f377b5d110ea87cd5964c59acc5c09e7cff
Hassan Andrabi <handrabi53@gmail.com>
2022-10-04 15:22:33 +1100

    Reguarlised network test.

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from src.estimate import estimate
 
 # All models feature an untrainable embedding layer by default
 models = {
+	"1_bi_directional_regularised" : [
+		Bidirectional(LSTM(100, activation="relu")),
+		Dense(128, activation="relu", kernel_regularizer="l2"),
+		Dense(1, activation="sigmoid"),
+	],
 	"1_bi_directional" : [
 		Bidirectional(LSTM(100, activation="relu")),
 		Dense(128, activation="relu"),
@@ -20,5 +25,5 @@ for name, layers in models.items():
 	estimate(
 		name=name,
 		layers=layers,
-		epochs=20,
+		epochs=10,
 	)
